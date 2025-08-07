a673c97b05f682a019b511c51a485ab2442a351c
John Chilton <jmchilton@gmail.com>
2025-08-07 13:03:20 -0400

    Rebuild schema...

--- client/src/api/schema/schema.ts
+++ client/src/api/schema/schema.ts
@@ -8178,7 +8178,7 @@ export interface components {
             annotation?: string | null;
             /**
              * Content
-             * @description Raw text contents of the last page revision (type dependent on content_format).
+             * @description Text contents of the last page revision with embedded directives expanded (type dependent on content_format).
              * @default
              */
             content: string | null;
@@ -17420,10 +17420,16 @@ export interface components {
             author_deleted: boolean;
             /**
              * Content
-             * @description Raw text contents of the last page revision (type dependent on content_format).
+             * @description Text contents of the last page revision with embedded directives expanded (type dependent on content_format).
              * @default
              */
             content: string | null;
+            /**
+             * Content for Editor
+             * @description Raw text contents of the last page revision (type dependent on content_format).
+             * @default
+             */
+            content_editor: string | null;
             /**
              * Content format
              * @description Either `markdown` or `html`.
@@ -20682,7 +20688,7 @@ export interface components {
         ToolReportForDataset: {
             /**
              * Content
-             * @description Raw text contents of the last page revision (type dependent on content_format).
+             * @description Text contents of the last page revision with embedded directives expanded (type dependent on content_format).
              * @default
              */
             content: string | null;
